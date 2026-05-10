e7f658c57832a6c0d09c5366c22500f9fbf695fb
evyatco2-debug <evyatco2@ac.sce.ac.il>
2026-05-10 15:41:11 +0300

    test_buckling.py

--- test_buckling.py
+++ test_buckling.py
@@ -1,12 +1,17 @@
 import numpy as np
-from scipy import optimize
-def column_stress_error(P, L, E, A, r, c, e, sigma_allow):
-    # פרמטרים לדוגמה 
-    #A, E, L, r, e, c = 5000, 200000, 3000, 50, 20, 100 
-    # נוסחת הסקנט
-    sec_term = 1 / np.cos((L/(2*r)) * np.sqrt(P/(E*A)))
-    sigma_max = (P/A) * (1 + (e*c/r**2) * sec_term)
-    return sigma_max - sigma_allow 
-def find_critical_load(L, E, A, r, c, e, sigma_allow):
-    P_critical = optimize.newton(lambda P: column_stress_error(P, L, E, A, r, c, e, sigma_allow), 500000)
-    return P_critical  
+import pytest
+from column_buckling import find_critical_load
+
+def test_known_case():
+    # נתונים עבור עמוד פלדה ספציפי שחישבת מראש
+    L, E, A, r, c, e, sigma_allow = 3000, 200000, 5000, 50, 100, 20, 250
+    result = find_critical_load(L, E, A, r, c, e, sigma_allow)
+    
+    # בדיקה שהתוצאה לא שלילית והיא הגיונית (למשל, קטנה מעומס אוילר)
+    euler_load = (np.pi**2 * E * (A * r**2)) / (L**2)
+    assert 0 < result < euler_load
+    
+    # בדיקה שהתוצאה מאפסת את המשוואה (בקירוב)
+    P = result
+    sigma_max = (P/A) * (1 + (e*c/r**2) * (1/np.cos((L/(2*r)) * np.sqrt(P/(E*A)))))
+    assert np.isclose(sigma_max, sigma_allow, rtol=1e-3)
